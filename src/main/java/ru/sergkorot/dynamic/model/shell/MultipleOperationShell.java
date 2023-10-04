d870392c3deb8e7ab56f400f38b6e088d4348456
Sergey Korotaev <sergey.evgen.kor2501@gmail.com>
2023-10-04 21:33:46 +0300

    #1 create core functionality for starters

---
+++ src/main/java/ru/sergkorot/dynamic/model/shell/MultipleOperationShell.java
@@ -0,0 +1,21 @@
+package ru.sergkorot.dynamic.model.shell;
+
+import lombok.Value;
+import ru.sergkorot.dynamic.model.ComplexSearchParam;
+import ru.sergkorot.dynamic.model.PageAttribute;
+import ru.sergkorot.dynamic.model.enums.GlueOperation;
+
+import java.util.List;
+
+import static ru.sergkorot.dynamic.model.enums.GlueOperation.AND;
+
+/**
+ * @author Sergey Korotaev
+ * Shell for complex search requests
+ */
+@Value
+public class MultipleOperationShell {
+    List<ComplexSearchParam> search;
+    GlueOperation externalGlue = AND;
+    PageAttribute pageAttribute;
+}
